56c389adf453e064a2e096ea54983bfabf300190
Arun <arun@instaastro.in>
2025-08-18 15:43:05 +0530

    dep

---
+++ .scripts/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+echo "🚀 Deployment started..."
+
+# Pull the latest version of the app
+git pull origin master
+echo "✅ New changes copied to server!"
+
+# Install dependencies
+echo "📦 Installing Dependencies..."
+npm install --yes
+
+# Create production build
+echo "🔨 Creating Production Build..."
+npm run build
+
+echo "🎉 Deployment Finished!"
